801f8977fff614b8247287b48d840be02b81c88d
Breval Le Floch <breval2000@live.fr>
2021-03-24 16:59:53 +0100

    Create Form Lot

--- src/Controller/MeController.php
+++ src/Controller/MeController.php
@@ -2,9 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Lot;
+use App\Entity\Produit;
 use App\Entity\User;
+use App\Form\NewLotType;
+use App\Form\ProduitType;
 use App\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -42,4 +48,34 @@ class MeController extends AbstractController
             'form' => $form->createView(),
         ]);
     }
+
+
+    /**
+     * @Route("/new", name="me_add_object", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+
+        $lot =new Lot();
+        $form = $this->createForm(NewLotType::class,$lot);
+
+
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($lot);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('produit_index');
+        }
+
+        return $this->render('produit/new.html.twig', [
+            'form' => $form
+        ]);
+    }
+
+
+
 }
---
+++ src/Form/NewLotType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Lot;
+use App\Entity\Produit;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class NewLotType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('Nom')
+            ->add('Description')
+            ->add('Produits',  CollectionType::class, [
+                // each entry in the array will be an "email" field
+                'entry_type' => ProduitType::class,
+                'allow_add' => true,
+                // these options are passed to each "email" type
+                'entry_options' => [
+                    'attr' => ['class' => 'email-box'],
+                ]])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Lot::class,
+        ]);
+    }
+}
